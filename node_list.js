c82356087251bc2be16a46aa4ce7ca083ac02e38
tanran <215920697@qq.com>
2018-11-15 11:06:58 +0800

    前台列表页ajax请求和后台接口完成

--- node_list.js
+++ node_list.js
@@ -12,9 +12,26 @@ const conn = mysql.createConnection({
 const app = express()
 
 app.use(bodyparser.urlencoded({extended: false}))
+
+//开启跨域
+const cors = require('cors');
+app.use(cors());
+//查询数据库的数据
+app.get("/",(req,res)=>{
+    const select = "select * from list"
+
+    conn.query(select,(err,data) => {
+        if(err) return res.send({status:500, msg:"获取数据失败"})
+
+        // console.log(data)
+        res.send({status:200, msg:"获取数据成功", data: data})
+    })
+})
+
 //添加数据到数据库
 app.post("/add",(req,res)=>{
     const list = req.body;
+    list.date = new Date()
     // console.log(list);
     const insert = "insert into list set ?"
 
@@ -29,12 +46,12 @@ app.post("/add",(req,res)=>{
 //删除数据
 app.get("/del/:id",(req,res) =>{
     const id = req.params;
-    // console.log(id)
+    // console.log(id.id)
     const deletes = "delete from list where id=?"
 
-    conn.query(deletes,id,(err,data) => {
+    conn.query(deletes,id.id,(err,data) => {
         if(err) return res.send({status: 500, msg:"删除失败"})
-        if(data.affectedRows !== 1) return res.send({status:400,msg:"删除失败啊"})
+        if(data.affectedRows !== 1) return res.send({status:400,msg:"删除失败啊",data:data})
         res.send({status:200, msg:"删除成功"})
     })
 
